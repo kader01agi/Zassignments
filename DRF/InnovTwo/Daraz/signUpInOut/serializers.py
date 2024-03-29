73bb17b9a294047dc36e92b74a89c1f684fbc51a
kader01agi <md.abdul.kader.ru@gmail.com>
2024-03-29 11:35:11 +0600

    serializer fields changed

--- DRF/InnovTwo/Daraz/signUpInOut/serializers.py
+++ DRF/InnovTwo/Daraz/signUpInOut/serializers.py
@@ -3,4 +3,5 @@ from .models import DarazUser
 class DarazUserSrializer(serializers.ModelSerializer):
     class Meta:
         model = DarazUser
-        fields =  ["full_name", "phone_number", "email", "date_of_birth"]
+        fields = "__all__"
+        # fields =  ["full_name", "phone_number", "email", "date_of_birth"]
